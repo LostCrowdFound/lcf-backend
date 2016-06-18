72f656fbf3b3614a1d5fb4306271c0fa7c94cc7c
Moritz <Moritz@Moritz-Desktop>
2016-06-18 14:20:25 +0200

    remove wrong gets

--- request/requestController.js
+++ request/requestController.js
@@ -13,37 +13,3 @@ exports.postRequest = function (req, res) {
   });
 };
 
-// Create endpoint /api/items for GET
-exports.getRequests = function (req, res) {
-  Request.find({
-    type: req.query.type,
-    brand: req.query.brand,
-    name: req.query.name,
-    date: { $gt: req.query.date },
-  }, '_id lat lon', function (err, items) {
-      if (err) {
-        return res.status(500).send(err);
-      }
-
-      var filteredItems = items.filter(isPointInRadius, req.query);
-
-      console.log('Returning items: ' + items);
-      res.status(200).json(filteredItems);
-    }
-  );
-};
-
-// Create endpoint /api/items/:item_id for GET
-exports.getItem = function (req, res) {
-  // if (!req.user.equals(movie.user)) {
-  //       res.sendStatus(401);
-  // }
-
-  Item.findById(req.params.item_id, 'email', function (err, item) {
-    if (err) {
-      return res.status(500).send(err);
-    };
-
-    res.json(item);
-  });
-};
