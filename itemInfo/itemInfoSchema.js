23efba38082b2570cf9d4f4da700bb1c6751da3a
Sönke Erfkamp <Snke Erfkamp>
2016-06-30 11:08:50 +0200

    add itemInfo folder

---
+++ itemInfo/itemInfoSchema.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var itemInfoSchema = mongoose.Schema({
+	type: {
+		    type: String,
+		    unique: true,
+		  },
+	brands: [{brand:  String, models: [String]}]	
+});
+
+var itemInfo = mongoose.model('itemInfo', itemInfoSchema);
+
+module.exports = itemInfo;
